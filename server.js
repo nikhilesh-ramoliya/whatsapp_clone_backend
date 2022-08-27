498d99d337a14ab5e4c122352b8459cbddb2d876
niskPatel1 <ramoliyanikhil252525@gmail.com>
2022-08-27 22:20:51 +0530

    first heroku push for try

--- server.js
+++ server.js
@@ -43,6 +43,8 @@ db.once("open", ()=>{
 app.use(Express.json());
 app.use(cors());
 app.use((req,res, next)=>{
+    res.seHeader("Access-Control-Allow-Origin", "http://localhost:3000/");
+    res.setHeader("Access-Control-Allow-Headers", "http://localhost:3000/");
     next();
 })
 
