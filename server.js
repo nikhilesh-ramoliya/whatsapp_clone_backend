0e4fff2d4e5d9ce9a704e6b2cbde002ef89dde5a
niskPatel1 <ramoliyanikhil252525@gmail.com>
2022-08-28 10:47:55 +0530

    problem solved 1

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //! import 
-import  Express  from "express";
+import Express from "express";
 import mongoose from "mongoose";
-import cors  from "cors";
+import cors from "cors";
 import dotenv from "dotenv";
 import Message from "./dbMessages.js";
 import Pusher from "pusher";
@@ -18,37 +18,50 @@ const pusher = new Pusher({
     secret: "32c7064e0bd1a6d058bc",
     cluster: "ap2",
     useTLS: true
-  });
+});
 
 const db = mongoose.connection
 
-db.once("open", ()=>{
+db.once("open", () => {
     console.log("db is connected by open");
     const msgCollection = db.collection("messagecontents");
     const changestream = msgCollection.watch();
 
-    changestream.on("change", (change)=>{
-        console.log("a chang occured", change);        
+    changestream.on("change", (change) => {
+        console.log("a chang occured", change);
         //!---------------------------------------------
         if (change.operationType === "insert") {
             const messageDetails = change.fullDocument;
             pusher.trigger("message", "inserted", messageDetails)
         } else {
-            console.log("error triggring pusher");
+            /* ----------------- console.log("error triggring pusher"); ----------------- */
+            const mes = {
+                name: "creater",
+                timestamp: new Date().toLocaleTimeString(),
+                message: "This is a first message created by creater of this app",
+                recieved: true
+            }
+            Message.create(mes, (req, res) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("done");
+                }
+            })
         }
         //!---------------------------------------------
     })
 })
-  
+
 //! middleware
 app.use(Express.json({ limit: '50mb' }));
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ limit:"50mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 //!  dbconfig
 const connection_url = "mongodb+srv://whatsapp:whatsapp@cluster0.d0haad1.mongodb.net/?retryWrites=true&w=majority"
-mongoose.connect(connection_url, (err)=>{
+mongoose.connect(connection_url, (err) => {
     if (!err) {
         console.log("database is connected");
     }
@@ -57,12 +70,12 @@ mongoose.connect(connection_url, (err)=>{
 //! ????
 
 //!api routs 
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
     res.status(200).send("hello world");
 })
-app.post("/messages/new", (req,res)=>{
+app.post("/messages/new", (req, res) => {
     const dbMessage = req.body
-    Message.create(dbMessage, (err,data)=>{
+    Message.create(dbMessage, (err, data) => {
         if (err) {
             res.status(500).send(err);
         } else {
@@ -70,8 +83,8 @@ app.post("/messages/new", (req,res)=>{
         }
     })
 })
-app.get("/messages/sync", (req,res)=>{
-    Message.find((err,data)=>{
+app.get("/messages/sync", (req, res) => {
+    Message.find((err, data) => {
         if (err) {
             res.status(500).send(err);
         } else {
@@ -81,6 +94,6 @@ app.get("/messages/sync", (req,res)=>{
 })
 
 //! listen
-app.listen(port, ()=>{
-    console.log("server is running : "+ port );
+app.listen(port, () => {
+    console.log("server is running : " + port);
 })
